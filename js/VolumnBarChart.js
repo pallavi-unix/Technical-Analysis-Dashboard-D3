e2f384a41ce4bf2af42ebbdf61d5d3a0b0eccff0
Pallavi Deshmukh <pallavideshmukh32897@gmail.com>
2024-04-25 16:50:14 -0400

    Labels issues fixed

--- js/VolumnBarChart.js
+++ js/VolumnBarChart.js
@@ -34,59 +34,64 @@ class VolumnBarChart {
 
     updateVis() {
         let vis = this;
-
-        vis.quarterlyVolumes = Array.from(
+    
+        // Convert the Date string to a JavaScript Date object
+        vis.data.forEach(d => {
+            d.Date = new Date(d.Date);
+        });
+    
+        // Calculate daily volumes
+        vis.dailyVolumes = Array.from(
             d3.rollup(vis.data, v => d3.mean(v, d => d.Volume), d => {
-                return `${d.Date}`;
+                return d3.timeFormat('%Y-%m-%d')(new Date(d.Date));
             }),
-            ([key, value]) => ({ Quarter: key, Volume: value })
+            ([key, value]) => ({ Date: new Date(key), Volume: value })
         );
-
-        const maxVolume = d3.max(vis.quarterlyVolumes, d => d.Volume);
+    
+        const maxVolume = d3.max(vis.dailyVolumes, d => d.Volume);
         const maxVolumeRoundUp = Math.ceil(maxVolume / 10000000) * 10000000;
-
+    
         vis.xScale = d3.scaleBand()
-            .domain(vis.quarterlyVolumes.map(d => d.Quarter))
+            .domain(vis.dailyVolumes.map(d => d.Date))
             .range([0, vis.width])
-            .padding(0.1);
-
+            .padding(0.2);
+    
         vis.yScale = d3.scaleLinear()
             .domain([0, maxVolumeRoundUp])
             .nice()
             .range([vis.height, 0]);
-
-        vis.xAxisCall = d3.axisBottom(vis.xScale);
-
+    
+        vis.xAxisCall = d3.axisBottom(vis.xScale)
+            .tickValues(vis.dailyVolumes.filter((d, i) => i % 30 === 0).map(d => d.Date))
+            .tickFormat(d3.timeFormat('%b %Y'));
+    
         vis.renderVis();
     }
-
+    
     renderVis() {
         let vis = this;
-
+    
         vis.chart.selectAll('.bar')
-            .data(vis.quarterlyVolumes)
+            .data(vis.dailyVolumes)
             .enter().append('rect')
             .attr('class', 'bar')
-            .attr('x', d => vis.xScale(d.Quarter))
+            .attr('x', d => vis.xScale(d.Date))
             .attr('width', vis.xScale.bandwidth())
             .attr('y', d => vis.yScale(d.Volume))
             .attr('height', d => vis.height - vis.yScale(d.Volume))
             .attr('fill', 'steelblue');
-
+    
         vis.chart.append('g')
+            .attr('class', 'x-axis')
             .attr('transform', `translate(0,${vis.height})`)
             .call(vis.xAxisCall)
             .selectAll('text')
             .attr('transform', 'rotate(-90)')
             .attr('text-anchor', 'end')
-            .style('font-size', '12px');
-
-        // Label for x-axis
-        vis.svg.append("text")
-            .attr("transform", `translate(${vis.width / 2},${vis.height + 130})`)
-            .style("text-anchor", "middle")
-            .text("Dates");
-
+            .attr('dx', '-0.5em')
+            .attr('dy', '0.5em')
+            .style('font-size', '10px');
+    
         // Label for y-axis
         vis.svg.append("text")
             .attr("transform", "rotate(-90)")
@@ -95,11 +100,15 @@ class VolumnBarChart {
             .attr("dy", "1em")
             .style("text-anchor", "middle")
             .text("Volume");
+    
+        vis.svg.append("text")
+            .attr("transform", `translate(${vis.width / 2},${vis.height + 150})`)
+            .style("text-anchor", "middle")
+            .text("Date");
 
         vis.chart.append('g')
             .call(d3.axisLeft(vis.yScale)
                 .tickValues(d3.range(0, vis.yScale.domain()[1] + 1, 10000000))
                 .tickFormat(d3.format(".2s")));
-
-    }
+    }   
 }
